cb159ff33d04244241667d80a8702fa536f7c869
Felipe Francischetto <felipefrancischetto@hotmail.com>
2017-12-18 11:54:00 -0200

    feature: adding new components about Dashboard.

---
+++ src/scenes/dashboard/components/Dashboard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import Graphics from './Graphics'
+import Metrics from './Metrics'
+
+class Dashboard extends Component {
+  render () {
+    return(
+      <div style={{background: 'black'}}>
+        <h1>Funciona Caralho</h1>
+      </div>
+    )
+  }
+}
+
+export default Dashboard
---
+++ src/scenes/dashboard/components/Graphics.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { Grid, Menu, Dropdown, Segment, Icon, Image, Statistic, Divider } from 'semantic-ui-react'
+
+export default class Graphics extends Component {
+  render () {
+
+    const employeeSelectorStyle = {
+      height: 60
+    }
+
+    const graphicStyle = {
+      background: 'linear-gradient(135deg, #7db9e8 0%,#1e5799 100%)',    
+      color: 'white !important',
+      height: 280
+    }
+
+    return (
+      <div>
+        <h1>Graph</h1>
+      </div>
+    )
+  }
+}
